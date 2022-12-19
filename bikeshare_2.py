10eb7720d9567843db9f807b4a7b497836037e6e
jegali <jens@jensgaulke.de>
2022-12-19 21:13:04 +0100

    Added comment to trip_duration_stats

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -200,6 +200,7 @@ def station_stats(df):
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
+    # third comment for github
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
